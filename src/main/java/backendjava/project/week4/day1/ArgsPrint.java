b05f59a1cffd39242c059cef772a1907b2077e4d
Taehwani-Storage <wiz_th1024@naver.com>
2023-05-08 15:52:42 +0900

    feat: Args 출력 - ArgsPrint 실습

---
+++ src/main/java/backendjava/project/week4/day1/ArgsPrint.java
@@ -0,0 +1,10 @@
+package backendjava.project.week4.day1;
+
+import java.util.Arrays;
+
+public class ArgsPrint {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
+
+    }
+}
