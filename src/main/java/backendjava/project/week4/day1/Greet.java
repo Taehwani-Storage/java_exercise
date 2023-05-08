31986cacb603f1d95c95e6cff52ed4c6d66ac068
Taehwani-Storage <wiz_th1024@naver.com>
2023-05-08 14:01:40 +0900

    feat: week4-day1 메소드 실습1 - Greet, GreetTest 실습

---
+++ src/main/java/backendjava/project/week4/day1/Greet.java
@@ -0,0 +1,13 @@
+package backendjava.project.week4.day1;
+
+public class Greet {
+//    private void printHello() {
+//        System.out.println("Hello");
+//    }
+//    public static void main(String[] args) {
+//
+//    }
+    public void printHello() {
+        System.out.println("Hello");
+    }
+}
---
+++ src/main/java/backendjava/project/week4/day1/GreetTest.java
@@ -0,0 +1,11 @@
+package backendjava.project.week4.day1;
+
+
+class GreetTest {
+    public static void main(String[] args) {
+        Greet greet = new Greet();
+        greet.printHello();
+
+    }
+
+}
