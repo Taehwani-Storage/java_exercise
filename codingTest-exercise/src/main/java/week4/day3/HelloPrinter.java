cd0da85734e97a7b9c091f9b299fc828fbf0353b
Taehwani-Storage <wiz_th1024@naver.com>
2023-05-10 14:33:24 +0900

    feat: 인터페이스 설명2 - HelloPrinter 실습 - Printer2 인터페이스 실습 - ConsolePrinter 구현체 실습

--- codingTest-exercise/src/main/java/week4/day3/HelloPrinter.java
+++ codingTest-exercise/src/main/java/week4/day3/HelloPrinter.java
@@ -1,14 +1,20 @@
 package week4.day3;
 
 public class HelloPrinter {
+    Printer2 printer; // OCP(개방 폐쇄 원칙)에 의해 확장만 되는 곳
+
 //    파일 저장, 콘솔 출력
     public void print(String message) {
         System.out.println(message);
     }
-
+//    기능 1. 반복하는 기능 추가
+    public void repeatMessage(int n, String message) {
+        for(int i = 0; i < n; i++) {
+            print(message);
+        }
+    }
     public static void main(String[] args) {
         HelloPrinter hp = new HelloPrinter();
-        hp.print("Hello");
-        hp.print("Bye");
+        hp.repeatMessage(5, "Hello");
     }
 }
---
+++ codingTest-exercise/src/main/java/week4/day3/Printer2.java
@@ -0,0 +1,5 @@
+package week4.day3;
+
+public interface Printer2 {
+    void print(String message);
+}
