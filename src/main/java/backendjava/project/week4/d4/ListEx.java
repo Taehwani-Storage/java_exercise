f65d543d49475fba303923940f4a3e08e5b19c67
Taehwani-Storage <wiz_th1024@naver.com>
2023-05-11 09:25:11 +0900

    feat: List 실습 - ListEx

---
+++ src/main/java/backendjava/project/week4/d4/ListEx.java
@@ -0,0 +1,24 @@
+package backendjava.project.week4.d4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListEx {
+    public static void main(String[] args) {
+        List l1 = new ArrayList();
+        l1.add("Hello");
+        l1.add(1);
+        System.out.println(l1.get(0));
+        System.out.println(l1.get(1));
+        System.out.println(l1.size());
+        System.out.println(l1.isEmpty());
+        l1.remove(0);
+        l1.remove(0);
+        System.out.println(l1.isEmpty());
+
+        int[] arr = new int[3];
+        arr[0] = 1;
+        arr[0] = 0; // 다시 0으로 초기화
+//        arr[0] = null; // 이건 불가능
+    }
+}
