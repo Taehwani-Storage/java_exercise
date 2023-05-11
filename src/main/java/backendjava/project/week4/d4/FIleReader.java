6b1bccec3c1fa6b4a615811710e32cc34b01d19a
Taehwani-Storage <wiz_th1024@naver.com>
2023-05-11 15:20:20 +0900

    feat: Hospital Object 만들기(Parser 프로젝트) - FileReader, Hospital 실습

---
+++ src/main/java/backendjava/project/week4/d4/FIleReader.java
@@ -0,0 +1,10 @@
+package backendjava.project.week4.d4;
+
+public class FIleReader {
+    public String[] read() {
+        return new String[5];
+    }
+    public static void main(String[] args) {
+
+    }
+}
