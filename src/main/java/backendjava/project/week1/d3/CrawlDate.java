897efa6b224cf91b15ad52a963a3228655640cb6
Taehwani-Storage <wiz_th1024@naver.com>
2023-05-14 14:23:58 +0900

    feat: 마지막 크롤 시간 작업1 - CrawlDate 도메인 추가

---
+++ src/main/java/backendjava/project/week1/d3/CrawlDate.java
@@ -0,0 +1,12 @@
+package backendjava.project.week1.d3;
+
+import java.time.LocalDateTime;
+
+public class CrawlDate {
+    private LocalDateTime lastCrawDateTime;
+
+    public CrawlDate(LocalDateTime lastCrawDateTime) {
+        this.lastCrawDateTime = lastCrawDateTime;
+    }
+
+}
